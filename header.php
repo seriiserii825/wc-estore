f1e4999105a6c8ace7165121384129eba743e7a6
serii <seriiburduja@gmail.com>
2020-07-01 18:17:58 +0300

    start ajax form

--- header.php
+++ header.php
@@ -70,8 +70,8 @@
                 <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
             </label>
             <div class="search_form">
-                <form action="#" method="post">
-                    <input type="text" name="Search" placeholder="Search...">
+                <form action="<?php esc_url(home_url('/')); ?>" method="post">
+                    <input type="text" name="s" value="<?php get_search_query(); ?>" placeholder="Search...">
                     <input type="submit" value="Send">
                 </form>
             </div>
