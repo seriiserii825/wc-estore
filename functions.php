18299e41084cc5215073dcaefa06d2c00cce1510
serii <seriiburduja@gmail.com>
2020-07-01 18:08:48 +0300

    logo

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '1.0.0' );
 }
 require_once __DIR__ . '/inc/func.php';
+require_once __DIR__ . '/inc/acf.php';
 require_once __DIR__ . '/inc/bs-setup.php';
 require_once __DIR__ . '/inc/bs-widgets.php';
 require_once __DIR__ . '/inc/bs-styles.php';
--- header.php
+++ header.php
@@ -28,11 +28,41 @@
             </a>
         </div>
         <div class="w3l_logo">
-            <h1>
-                <a href="index.html">Electronic Store
-                    <span>Your stores. Your place.</span>
-                </a>
-            </h1>
+			<?php
+			$logo          = get_field( 'logo', 'option' );
+			$logo_select   = $logo['logo_select'] ? $logo['logo_select'] : '';
+			$logo_image    = $logo['logo_image'] ? $logo['logo_image'] : '';
+			$logo_title    = $logo['logo_title'] ? $logo['logo_title'] : '';
+			$logo_subtitle = $logo['logo_subtitle'] ? $logo['logo_subtitle'] : '';
+			?>
+
+			<?php if ( is_front_page() ): ?>
+				<?php if ( $logo_select === 'Choose text' ): ?>
+                    <h1>
+                        <span><?php echo $logo_title; ?></span>
+                        <span><?php echo $logo_subtitle; ?></span>
+                    </h1>
+				<?php else: ?>
+                    <h1>
+                        <img src="<?php echo $logo_image; ?>" width="300" alt="">
+                    </h1>
+				<?php endif; ?>
+			<?php else: ?>
+				<?php if ( $logo_select === 'Choose text' ): ?>
+                    <h1>
+                        <a href="<?php echo home_url(); ?>">
+                            <span><?php echo $logo_title; ?></span>
+                            <span><?php echo $logo_subtitle; ?></span>
+                        </a>
+                    </h1>
+				<?php else: ?>
+                    <h1>
+                        <a href="<?php echo home_url(); ?>">
+                            <img src="<?php echo $logo_image; ?>" width="300" alt="">
+                        </a>
+                    </h1>
+				<?php endif; ?>
+			<?php endif; ?>
         </div>
         <div class="search">
             <input class="search_box" type="checkbox" id="search_box">
