36214b520bfbb69b6ed5b64c8d7970c7bccdf4e5
serii <seriiburduja@gmail.com>
2020-07-03 14:04:44 +0300

    some

--- single.php
+++ single.php
@@ -6,35 +6,22 @@
  *
  * @package wc-estore
  */
-
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'wc-estore' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'wc-estore' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+    <div class="container">
+        <div class="page-content-wrap">
+            <div class="page-content">
+				<?php while ( have_posts() ) :
+					the_post();
+					the_content();
+				endwhile; // End of the loop. ?>
+            </div>
+            <div class="sidebar">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
 
 <?php
-get_sidebar();
 get_footer();
